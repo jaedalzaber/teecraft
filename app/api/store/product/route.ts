b70d761d377181cdc31f749ba1ae7bdb06147bea
jaedalzaber <jaedalzaber@gmail.com>
2025-10-17 22:12:33 +0600

    .

--- app/api/store/product/route.ts
+++ app/api/store/product/route.ts
@@ -1,4 +1,4 @@
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { authSeller } from "@/middlewares/authSeller"
 import { NextResponse } from "next/server";
 import { handleUpload } from "@/configs/imageKit";
